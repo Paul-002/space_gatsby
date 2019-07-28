03d9f2450977db8167acbb6f53b9b8ab2bdc34f4
Paul <gasior002@gmail.com>
2019-07-29 00:44:32 +0200

    Rewrite upcomingLaunch to useStaticQuery hook, fix typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import UpcomingLaunch from "../components/upcomingLunch"
+import UpcomingLaunch from "./upcomingLunch"
 
 const IndexPage = () => {
   return (
