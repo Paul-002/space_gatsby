fb4daca15e8fa556d11186c16b17cd759290cd39
Paul <gasior002@gmail.com>
2019-08-07 14:19:28 +0200

    fix for netfly

--- gatsby-node.js
+++ gatsby-node.js
@@ -32,7 +32,7 @@ exports.createPages = ({ graphql, actions }) => {
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
-        component: path.resolve(`./src/templates/templatePost.js`),
+        component: path.resolve(`src/templates/templatePost.js`),
         context: {
           slug: node.fields.slug,
         },
